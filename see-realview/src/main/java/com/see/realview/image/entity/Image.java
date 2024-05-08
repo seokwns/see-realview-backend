80fb3be3d4f335f694731215544fa025e9f4846c
seokwns <seokjun0915@icloud.com>
2024-05-08 21:00:57 +0900

    feat: change image table schema
    - 이미지 분석 정보 저장 테이블에서 createdAt, updatedAt 필드 추가

--- see-realview/src/main/java/com/see/realview/image/entity/Image.java
+++ see-realview/src/main/java/com/see/realview/image/entity/Image.java
@@ -5,6 +5,8 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Entity
 @Table(name = "image_tb")
 @Getter
@@ -21,12 +23,20 @@ public class Image {
     @Column(nullable = false)
     private Long count;
 
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column
+    private LocalDateTime updatedAt;
+
 
     @Builder
-    public Image(String link, Boolean advertisement, Long count) {
+    public Image(String link, Boolean advertisement, Long count, LocalDateTime createdAt, LocalDateTime updatedAt) {
         this.link = link;
         this.advertisement = advertisement;
         this.count = count;
+        this.createdAt = (createdAt == null? LocalDateTime.now() : createdAt);
+        this.updatedAt = updatedAt;
     }
 
     public static Image of(String link, Boolean advertisement) {
@@ -34,6 +44,7 @@ public class Image {
                 .link(link)
                 .advertisement(advertisement)
                 .count(1L) // batch update 시에 중복 레코드는 count + 1로 업데이트하기 때문에 디폴트는 항상 1로 고정
+                .createdAt(LocalDateTime.now())
                 .build();
     }
 
